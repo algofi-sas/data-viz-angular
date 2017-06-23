ac6bcaa9002484640c10daad9ed431adccf1bf1c
Bilal EL CHAMI <belchami@algofi.local>
2017-06-23 13:57:58 +0200

    test

--- web.js
+++ web.js
@@ -7,5 +7,6 @@ app.use(morgan('dev'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 
 app.locals.DOMAIN_URL = process.env.DOMAIN_URL;
+console.log(process.env.DOMAIN_URL);
 
 app.listen(process.env.PORT || 5000);
