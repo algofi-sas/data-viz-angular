096b22888c238f6d8e629d658ca43f9846764abc
Bilal EL CHAMI <belchami@algofi.local>
2017-06-23 13:39:22 +0200

    testing envvars

--- web.js
+++ web.js
@@ -5,4 +5,9 @@ var app = express();
 
 app.use(morgan('dev'));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+
+app.get('/site.js', function(req, res){
+	res.send("var DOMAIN_URL='"+process.env.DOMAIN_URL+"'");
+});
+
 app.listen(process.env.PORT || 5000);
